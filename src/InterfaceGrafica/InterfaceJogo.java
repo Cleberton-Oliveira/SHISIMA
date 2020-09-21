4ee05e5d939f012ce924e5043aec5c32b2e77a53
mickaelcampos <camposmrc01@gmail.com>
2020-09-20 21:34:08 -0300

    feat: elementos da interface gráfica

--- src/InterfaceGrafica/InterfaceJogo.java
+++ src/InterfaceGrafica/InterfaceJogo.java
@@ -4,6 +4,7 @@ import java.awt.EventQueue;
 import java.awt.Rectangle;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
@@ -12,6 +13,8 @@ import javax.swing.JMenu;
 import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
 import javax.swing.Action;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 
 public class InterfaceJogo { // InterfaceShisima
 
@@ -51,7 +54,7 @@ public class InterfaceJogo { // InterfaceShisima
 		atorJogador = new AtorJogador();
 		
 		frame = new JFrame();
-		frame.setBounds(100, 100, 600, 600);
+		frame.setBounds(100, 100, 650, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setTitle("Shisima");
@@ -59,8 +62,7 @@ public class InterfaceJogo { // InterfaceShisima
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBounds(0, 0, 57, 21);
 		frame.setJMenuBar(menuBar);
-//		frame.getContentPane().add(menuBar);
-		
+			
 		JMenu mnNewMenu = new JMenu("Jogo");
 		mnNewMenu.setBounds(new Rectangle(1, 0, 57, 21));
 		menuBar.add(mnNewMenu);
@@ -76,6 +78,60 @@ public class InterfaceJogo { // InterfaceShisima
 		JMenuItem mntmIniciarPartida = new JMenuItem("iniciar partida");
 		mntmIniciarPartida.setAction(action_2);
 		mnNewMenu.add(mntmIniciarPartida);
+		
+		
+		
+		
+		Icon vazio = new ImageIcon(getClass().getResource("vazio.png"));
+		Icon azul = new ImageIcon(getClass().getResource("azul.png"));
+		Icon vermelho = new ImageIcon(getClass().getResource("vermelho.png"));
+		Icon template = new ImageIcon(getClass().getResource("template.png"));
+
+		
+		JLabel background = new JLabel();
+		background.setBounds(new Rectangle(20, 40, 600, 600));
+		background.setIcon(template);				
+		background.addMouseListener(new java.awt.event.MouseAdapter() {
+			public void mouseClicked(java.awt.event.MouseEvent e) {
+//				click(1,1); 
+			}
+		});
+
+		
+		JLabel vPosicao0 = new JLabel();
+		vPosicao0.setBounds(new Rectangle(20, 40, 190, 170));
+		vPosicao0.setIcon(vazio);				
+		vPosicao0.addMouseListener(new java.awt.event.MouseAdapter() {
+			public void mouseClicked(java.awt.event.MouseEvent e) {
+//				click(1,1); 
+			}
+		});
+		
+		JLabel vPosicao1 = new JLabel();
+		vPosicao1.setBounds(new Rectangle(90, 40, 190, 170));
+		vPosicao1.setIcon(azul);				
+		vPosicao1.addMouseListener(new java.awt.event.MouseAdapter() {
+			public void mouseClicked(java.awt.event.MouseEvent e) {
+//				click(1,1); 
+			}
+		});
+//		
+		JLabel vPosicao2 = new JLabel();
+		vPosicao2.setBounds(new Rectangle(200, 40, 190, 170));
+		vPosicao2.setIcon(vermelho);				
+		vPosicao2.addMouseListener(new java.awt.event.MouseAdapter() {
+			public void mouseClicked(java.awt.event.MouseEvent e) {
+//				click(1,1); 
+			}
+		});
+		
+//		JLabel VetorPosicao[] = new JLabel[9];
+		frame.getContentPane().add(vPosicao0);
+		frame.getContentPane().add(vPosicao1);
+		frame.getContentPane().add(vPosicao2);
+		frame.getContentPane().add(background);
+
+
 	}
 	private class SwingAction extends AbstractAction {
 		/**
